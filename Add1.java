7002b2a538adbc44bc19fe71589f83cec63d5c4d
shalud122 <59112219+shalud122@users.noreply.github.com>
2020-01-04 14:57:39 +0530

    Add files via upload

---
+++ Add1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Add1 {
+
+    public static void main(String[] args) {
+        
+        int num1=5;
+        int  num2=5; 
+        int sum;
+        sum=num1+num2;
+        System.out.println("sum is :"+sum);
+}
+}
